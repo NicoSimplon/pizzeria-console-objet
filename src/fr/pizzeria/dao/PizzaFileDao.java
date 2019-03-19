99ae426fe08098bec05c5fce22652251511ab8d3
Nicolas <marty.nicolas11@laposte.net>
2019-03-19 17:53:57 +0100

    Ajout gestion de persistance via fichier en cours

--- src/fr/pizzeria/dao/PizzaFileDao.java
+++ src/fr/pizzeria/dao/PizzaFileDao.java
@@ -7,6 +7,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
 
 /**
  * Système de CRUD avec écriture dans un fichier
@@ -16,35 +18,6 @@ import java.io.FileReader;
  */
 public class PizzaFileDao implements IPizzaDao{
 
-	private void initializer() {
-		Pizza[] tableauPizza = new Pizza[] {
-				new Pizza(0, "PEP", "Pépéroni", 12.50),
-				new Pizza(1, "MAR", "Margherita", 14.00),
-				new Pizza(2, "REIN", "La Reine", 11.50),
-				new Pizza(3, "FRO", "Les 4 fromages", 12.00),
-				new Pizza(4, "CAN", "La canibale", 12.50),
-				new Pizza(5, "SAV", "La savoyarde", 13.00),
-				new Pizza(6, "ORI", "L'orientale", 13.50),
-				new Pizza(7, "IND", "L'indienne", 14.00)
-		};
-	}
-	
-	private void lireFichierRech(Pizza tab[],String nFichier){
-	    String NomFichier = "C:\\TEMP\\workspace\\pizzeria-console-objet" + nFichier;
-	    try{
-	    	BufferedReader in  = new BufferedReader(new FileReader(NomFichier));
-	    	String line;
-	        int i = 0;
-	        while ((line = in.readLine()) != null) {
-	          tab[i] = Integer.parseInt(line.trim());
-	        i++;
-	        }
-	        in.close();
-	    }
-	    catch(Exception e){
-	    	e.printStackTrace();
-	    }
-	}
 
 	@Override
 	public Pizza[] findAllPizzas() {
