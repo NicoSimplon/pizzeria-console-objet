2e2fa8d4adcf32cd40a0b53f27b03750e24a4c1b
Nicolas <33360039+NicoSimplon@users.noreply.github.com>
2019-03-21 07:58:59 +0100

    bug dans la gestion de fichiers

--- src/fr/pizzeria/dao/PizzaFileDao.java
+++ src/fr/pizzeria/dao/PizzaFileDao.java
@@ -12,114 +12,95 @@ import fr.pizzeria.model.Pizza;
  *
  */
 public class PizzaFileDao implements IPizzaDao {
-	
-	private Pizza[] tableauPizza;
+
+	private List<Pizza> tableauPizza;
 	private GestionFichier gestionFichier;
-	
+
 	public PizzaFileDao() {
-		this.tableauPizza = new Pizza[10];
-		//initialisation();
-		gestionFichier = new GestionFichier ();
-		lecture ();
+		// Initialisation de l'ArrayList
+		this.tableauPizza = new ArrayList<>();
+
+		// Données Initiale (A commenter après l'initialisation du fichier .txt)
+		// initialisation();
+
+		// Initialisation du fichier save.txt
+		gestionFichier = new GestionFichier();
+		lecture();
 	}
-	
-	public void initialisation ()
-	{
-		this.tableauPizza = new Pizza[] {
-			new Pizza(0, "PEP", "Pépéroni", 12.50),
-			new Pizza(1, "MAR", "Margherita", 14.00),
-			new Pizza(2, "REIN", "La Reine", 11.50),
-			new Pizza(3, "FRO", "Les 4 fromages", 12.00),
-			new Pizza(4, "CAN", "La canibale", 12.50),
-			new Pizza(5, "SAV", "La savoyarde", 13.00),
-			new Pizza(6, "ORI", "L'orientale", 13.50),
-			new Pizza(7, "IND", "L'indienne", 14.00)
-		};
+
+	/**
+	 * Initialise les données de base de la liste de pizzas
+	 */
+	public void initialisation() {
+		tableauPizza.add(new Pizza(0, "PEP", "Pépéroni", 12.50));
+		tableauPizza.add(new Pizza(1, "MAR", "Margherita", 14.00));
+		tableauPizza.add(new Pizza(2, "REIN", "La Reine", 11.50));
+		tableauPizza.add(new Pizza(3, "FRO", "Les 4 fromages", 12.00));
+		tableauPizza.add(new Pizza(4, "CAN", "La canibale", 12.50));
+		tableauPizza.add(new Pizza(5, "SAV", "La savoyarde", 13.00));
+		tableauPizza.add(new Pizza(6, "ORI", "L'orientale", 13.50));
+		tableauPizza.add(new Pizza(7, "IND", "L'indienne", 14.00));
 	}
 
 	@Override
-	public Pizza[] findAllPizzas() {
+	public List<Pizza> findAllPizzas() {
 		return this.tableauPizza;
 	}
 
 	@Override
 	public void saveNewPizza(Pizza pizza) {
-		
-		boolean vide = false;
-		
-		for (int i = 0; i < this.tableauPizza.length; i++) {
-			if (this.tableauPizza[i] == null) {
-				this.tableauPizza[i] = pizza;
-				vide = true;
-				break;
-			}
-			else {
-				vide = false;
-			}
-		}
-		
-		if (vide == false) {
-			Pizza[] tab2 = new Pizza[this.tableauPizza.length + 1];
-			
-			for (int j = 0; j < this.tableauPizza.length; j++) {
-				tab2[j] = this.tableauPizza[j];
-			}
-			
-			this.tableauPizza = tab2;
-			
-			this.tableauPizza[this.tableauPizza.length - 1] = pizza;
-			
-		}
-		
+
+		this.tableauPizza.add(pizza);
+
 		ecriture();
-		
+
 	}
 
 	@Override
 	public void updatePizza(String codePizza, Pizza pizza) {
-	
-		for (int i = 0; i < this.tableauPizza.length; i++) {
-			if (this.tableauPizza[i].getCode().equals(codePizza)) {
-				tableauPizza[i] = pizza;
+
+		for (int i = 0; i < this.tableauPizza.size(); i++) {
+			if (this.tableauPizza.get(i).getCode().equals(codePizza)) {
+				tableauPizza.set(i, pizza);
 			}
 		}
-		
+
 		ecriture();
-		
+
 	}
 
 	@Override
 	public void deletePizza(String codePizza) {
-		
-		for (int i = 0; i < this.tableauPizza.length; i++) {
-			if (this.tableauPizza[i].getCode().equals(codePizza)) {
-				this.tableauPizza[i] = null;
+
+		for (int i = 0; i < this.tableauPizza.size(); i++) {
+			if (this.tableauPizza.get(i).getCode().equals(codePizza)) {
+				this.tableauPizza.remove(i);
 			}
 		}
-		
+
 		ecriture();
-		
+
 	}
 
 	@Override
 	public Pizza findPizzaByCode(String codePizza) {
-		
-		int idx = 0;
-		
-		for (int i = 0; i < this.tableauPizza.length; i++) {
-			if (this.tableauPizza[i].getCode().equals(codePizza)) {
-				idx = i;
+
+		Pizza p = null;
+
+		for (Pizza pizz : this.tableauPizza) {
+			if (pizz.getCode().equals(codePizza)) {
+				p = pizz;
 			}
 		}
-		
-		return this.tableauPizza[idx];
+
+		return p;
 	}
 
 	@Override
 	public boolean pizzaExists(String codePizza) {
 		boolean exist = false;
-		for (int i = 0; i < this.tableauPizza.length; i++) {
-			if (this.tableauPizza[i].getCode().equals(codePizza)) {
+		for (int i = 0; i < this.tableauPizza.size(); i++) {
+			if (this.tableauPizza.get(i).getCode().equals(codePizza)) {
 				exist = true;
 				break;
 			}
@@ -130,30 +111,30 @@ public class PizzaFileDao implements IPizzaDao {
 	/**
 	 * Ecrit dans un fichier .txt les données des pizzas
 	 */
-	public void ecriture () {
-		
-		List <String> listString = new ArrayList<String> ();
-		
+	public void ecriture() {
+
+		List<String> listString = new ArrayList<String>();
+
 		for (Pizza pizza : tableauPizza)
 			if (pizza != null)
 				listString.add(pizza.toSave());
-		
+
 		gestionFichier.ecriture(listString);
 	}
-	
+
 	/**
 	 * Lis les données des pizzas dans un fichier .txt
 	 */
-	public void lecture () {
-		
-		List <String> listString = gestionFichier.lecture();
-		
-		for (String s : listString)
-		{
-			String [] pizza = s.split(",");
-			tableauPizza[Integer.parseInt(pizza[0])] = new Pizza (Integer.parseInt(pizza[0]), pizza[1], pizza[2], Double.parseDouble(pizza[3]));
+	public void lecture() {
+
+		List<String> listString = gestionFichier.lecture();
+
+		for (String s : listString) {
+			String[] pizza = s.split(",");
+			this.tableauPizza.add(Integer.parseInt(pizza[0]),
+					new Pizza(Integer.parseInt(pizza[0]), pizza[1], pizza[2], Double.parseDouble(pizza[3])));
 		}
-			
+
 	}
-	
+
 }
--- src/fr/pizzeria/dao/PizzaMemDao.java
+++ src/fr/pizzeria/dao/PizzaMemDao.java
@@ -11,21 +11,11 @@ import fr.pizzeria.model.Pizza;
  * @author Nicolas
  *
  */
-public class PizzaMemDao implements IPizzaDao{
-	
+public class PizzaMemDao implements IPizzaDao {
+
 	private List<Pizza> tableauPizza;
-	
+
 	public PizzaMemDao() {
-//		this.tableauPizza = new Pizza[] {
-//				new Pizza(0, "PEP", "Pépéroni", 12.50),
-//				new Pizza(1, "MAR", "Margherita", 14.00),
-//				new Pizza(2, "REIN", "La Reine", 11.50),
-//				new Pizza(3, "FRO", "Les 4 fromages", 12.00),
-//				new Pizza(4, "CAN", "La canibale", 12.50),
-//				new Pizza(5, "SAV", "La savoyarde", 13.00),
-//				new Pizza(6, "ORI", "L'orientale", 13.50),
-//				new Pizza(7, "IND", "L'indienne", 14.00)
-//		};
 		List<Pizza> tableauPizza = new ArrayList<>();
 		tableauPizza.add(new Pizza(0, "PEP", "Pépéroni", 12.50));
 		tableauPizza.add(new Pizza(1, "MAR", "Margherita", 14.00));
@@ -44,62 +34,61 @@ public class PizzaMemDao implements IPizzaDao{
 
 	@Override
 	public void saveNewPizza(Pizza pizza) {
-		
+
 		boolean vide = false;
-		
+
 		for (int i = 0; i < this.tableauPizza.size(); i++) {
 			if (this.tableauPizza.get(i) == null) {
 				this.tableauPizza.set(i, pizza);
 				vide = true;
 				break;
-			}
-			else {
+			} else {
 				vide = false;
 			}
 		}
-		
+
 		if (vide == false) {
-			
+
 			this.tableauPizza.add(pizza);
-			
+
 		}
-		
+
 	}
 
 	@Override
 	public void updatePizza(String codePizza, Pizza pizza) {
-	
+
 		for (int i = 0; i < this.tableauPizza.size(); i++) {
 			if (this.tableauPizza.get(i).getCode().equals(codePizza)) {
 				tableauPizza.set(i, pizza);
 			}
 		}
-		
+
 	}
 
 	@Override
 	public void deletePizza(String codePizza) {
-		
+
 		for (int i = 0; i < this.tableauPizza.size(); i++) {
 			if (this.tableauPizza.get(i).getCode().equals(codePizza)) {
 				this.tableauPizza.remove(i);
 			}
 		}
-		
+
 	}
 
 	@Override
 	public Pizza findPizzaByCode(String codePizza) {
-		
+
 		Pizza p = null;
-		
+
 		for (Pizza piz : this.tableauPizza) {
 			if (piz.getCode().equals(codePizza)) {
 				p = piz;
 			}
-			
+
 		}
-		
+
 		return p;
 	}
 
