24683f06498c6c7f93480b6a22774ada7b981676
Nicolas <33360039+NicoSimplon@users.noreply.github.com>
2019-04-05 07:26:41 +0200

    Modification du fichier de sauvegarde de la base de données

--- src/main/resources/bdd_pizzeria.sql
+++ src/main/resources/bdd_pizzeria.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.4
+-- version 4.7.4
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  jeu. 04 avr. 2019 à 09:55
--- Version du serveur :  5.7.24
--- Version de PHP :  7.2.14
+-- Généré le :  ven. 05 avr. 2019 à 05:25
+-- Version du serveur :  5.7.19
+-- Version de PHP :  7.1.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -32,28 +32,27 @@ DROP TABLE IF EXISTS `pizzas`;
 CREATE TABLE IF NOT EXISTS `pizzas` (
   `id` int(6) NOT NULL AUTO_INCREMENT,
   `code` varchar(3) NOT NULL,
-  `libelle` varchar(30) NOT NULL,
+  `libelle` varchar(60) NOT NULL,
   `prix` double NOT NULL,
-  `categorie` varchar(30) NOT NULL,
+  `categorie` enum('Viande','Poisson','Sans_Viande','Default') NOT NULL DEFAULT 'Default',
   PRIMARY KEY (`id`),
   UNIQUE KEY `code` (`code`)
-) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=32 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `pizzas`
 --
 
 INSERT INTO `pizzas` (`id`, `code`, `libelle`, `prix`, `categorie`) VALUES
-(1, 'PEP', 'Pépéroni', 12.5, 'Viande'),
-(2, 'MAR', 'Margherita', 14, 'Viande'),
-(3, 'REI', 'Les 4 fromages', 11.5, 'Viande'),
-(4, 'FRO', 'La canibale', 12, 'Viande'),
-(5, 'CAN', 'La canibale', 12.5, 'Viande'),
-(6, 'SAV', 'La savoyarde', 13, 'Viande'),
-(7, 'ORI', 'L\'orientale', 13.5, 'Viande'),
-(8, 'IND', 'L\'indienne', 14, 'Viande'),
-(9, 'AZE', 'Azénor', 15, 'Viande'),
-(11, 'AZR', 'Azrnon', 50, 'Viande');
+(22, 'PEP', 'Pépéroni', 12.5, 'Viande'),
+(23, 'MAR', 'Margherita', 14, 'Viande'),
+(24, 'REI', 'La Reine', 11.5, 'Viande'),
+(25, 'FRO', 'Les 4 fromages', 12, 'Sans_Viande'),
+(26, 'CAN', 'La canibale', 12.5, 'Viande'),
+(27, 'SAV', 'La savoyarde', 13, 'Viande'),
+(28, 'ORI', 'L\'orientale', 13.5, 'Viande'),
+(29, 'IND', 'L\'indienne', 14, 'Poisson'),
+(31, 'AZA', 'Azara', 15, 'Default');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
