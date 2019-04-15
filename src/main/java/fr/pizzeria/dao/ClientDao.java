7678db129e5efa33f4aebbe8574c40ccd53d48fa
Nicolas <marty.nicolas11@laposte.net>
2019-04-15 09:38:46 +0200

    Début implémentation ClientDao

--- src/main/java/fr/pizzeria/dao/ClientDao.java
+++ src/main/java/fr/pizzeria/dao/ClientDao.java
@@ -1,11 +1,60 @@
 package fr.pizzeria.dao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
 import fr.pizzeria.model.Client;
 
 public class ClientDao {
 
+	private static EntityManagerFactory emFactory = Persistence.createEntityManagerFactory("pizzeria-complete");
+
+	private EntityManager openConnexion() {
+
+		return emFactory.createEntityManager();
+
+	}
+
+	private void closeConnexion(EntityManager em) {
+
+		em.close();
+
+	}
+
+	public void destroyEmFactory() {
+
+		emFactory.close();
+
+	}
+
 	public void saveNewClient(Client client) {
+
+		EntityManager em = openConnexion();
+		EntityTransaction et = em.getTransaction();
+
+		if(this.emailDontExist(client.getEmail())) {
+			et.begin();			
+			em.persist(client);
+			et.commit();	
+		}
+
+		closeConnexion(em);
 		
 	}
+
+	public boolean isValid(String email, String password) {
+
+		boolean validation = false;
+
+		return validation;
+	}
 	
+	public boolean emailDontExist(String amail) {
+		boolean dontExist = false;
+
+		return dontExist;
+	}
+
 }
--- src/main/java/fr/pizzeria/menuclient/ConnexionClientService.java
+++ src/main/java/fr/pizzeria/menuclient/ConnexionClientService.java
@@ -2,14 +2,36 @@ package fr.pizzeria.menuclient;
 
 import java.util.Scanner;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import fr.pizzeria.client.PizzeriaClientConsoleApp;
 import fr.pizzeria.dao.ClientDao;
 import fr.pizzeria.exception.StockageException;
 
 public class ConnexionClientService extends MenuClientService {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(PizzeriaClientConsoleApp.class);
 
 	@Override
 	public void executeUC(Scanner scanner, ClientDao dao) throws StockageException {
-		// TODO Auto-generated method stub
+		
+		LOGGER.info("Connexion");
+		LOGGER.info(" ");
+		
+		LOGGER.info("Veuillez renseigner votre email");
+		LOGGER.info("-------------------------------");
+		String email = scanner.nextLine();
+		
+		LOGGER.info("Veuillez renseigner votre mot de passe");
+		LOGGER.info("--------------------------------------");
+		String password = scanner.nextLine();
+		
+		if (dao.isValid(email, password)) {
+			LOGGER.info("Connecté");
+		}
+		
+		
 
 	}
 
